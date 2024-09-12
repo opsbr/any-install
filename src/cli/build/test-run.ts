e2a3874237e40b9abd9fcccf48b77c516bb057ac
Ryosuke Iwanaga <riywo@opsbr.com>
2024-09-12 12:38:12 -0700

    feat: enhance build commands with --test-* (#8)
    - Add the installDir to PATH automatically
    - Automatically cleanup the installDir

--- src/cli/build/test-run.ts
+++ src/cli/build/test-run.ts
@@ -1,9 +1,9 @@
 import { execFileSync } from "node:child_process";
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
-import { temporaryDirectory } from "tempy";
+import { temporaryDirectoryTask } from "tempy";
 
-export const testRun = (
+export const testRun = async (
   outDir: string,
   {
     sh,
@@ -12,38 +12,47 @@ export const testRun = (
     sh?: { file: string; env: string };
     ps1?: { file: string; env: string };
   },
-) => {
-  const tempDir = temporaryDirectory();
-  if (sh) {
-    const script = readFileSync(join(outDir, sh.file));
-    const installDir = join(tempDir, "sh");
-    execFileSync("sh", {
-      input: script,
-      env: {
-        ...process.env,
-        [sh.env]: installDir,
-      },
-      stdio: ["pipe", "inherit", "inherit"],
-    });
-    execFileSync(process.env.SHELL ?? "sh", {
-      cwd: installDir,
-      stdio: ["inherit", "inherit", "inherit"],
-    });
-  }
-  if (ps1) {
-    const script = readFileSync(join(outDir, ps1.file));
-    const installDir = join(tempDir, "ps1");
-    execFileSync("pwsh", ["-c", "$Input | Out-String | iex"], {
-      input: script,
-      env: {
-        ...process.env,
-        [ps1.env]: installDir,
-      },
-      stdio: ["pipe", "inherit", "inherit"],
-    });
-    execFileSync("pwsh", {
-      cwd: installDir,
-      stdio: ["inherit", "inherit", "inherit"],
-    });
-  }
-};
+) =>
+  temporaryDirectoryTask((tempDir) => {
+    if (sh) {
+      const script = readFileSync(join(outDir, sh.file));
+      const installDir = join(tempDir, "sh");
+      execFileSync("sh", {
+        input: script,
+        env: {
+          ...process.env,
+          [sh.env]: installDir,
+        },
+        stdio: ["pipe", "inherit", "inherit"],
+      });
+      execFileSync(process.env.SHELL ?? "sh", {
+        cwd: installDir,
+        env: {
+          ...process.env,
+          PATH: `${installDir}:${process.env.PATH}`,
+        },
+        stdio: ["inherit", "inherit", "inherit"],
+      });
+    }
+    if (ps1) {
+      const script = readFileSync(join(outDir, ps1.file));
+      const installDir = join(tempDir, "ps1");
+      execFileSync("pwsh", ["-c", "$Input | Out-String | iex"], {
+        input: script,
+        env: {
+          ...process.env,
+          [ps1.env]: installDir,
+        },
+        stdio: ["pipe", "inherit", "inherit"],
+      });
+      execFileSync("pwsh", {
+        cwd: installDir,
+        env: {
+          ...process.env,
+          PATH: `${installDir}:${process.env.PATH}`,
+          Path: `${installDir};${process.env.Path}`,
+        },
+        stdio: ["inherit", "inherit", "inherit"],
+      });
+    }
+  });
